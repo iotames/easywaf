24a0aaa34d1165659fff719a22dba3832942aacf
Hankin <554553400@qq.com>
2025-10-13 18:00:41 +0800

    放过包含的PHP请求头，避免误伤一加手机：OnePlus Ace 2 (PHP110)

--- filter/useragent.go
+++ filter/useragent.go
@@ -33,8 +33,9 @@ func compileSuspiciousPatterns() []*regexp.Regexp {
 	patterns := []string{
 		`(?i)(bot|scrapy|crawler|spider|scanner|baiduspider|sogou|yandex|duckduckbot|slurp|bingbot)`,
 		`(?i)(sqlmap|nmap|metasploit|nikto|acunetix|nessus)`,
-		`(?i)(curl|wget|python|java|requests|ruby|perl|php|httpclient|libwww-perl|okhttp|go-http-client|phantomjs|headless|fetch|axios|http_request2|http_request|http_get|http_post)`,
+		`(?i)(curl|wget|python|java|requests|ruby|perl|httpclient|libwww-perl|okhttp|go-http-client|phantomjs|headless|fetch|axios|http_request2|http_request|http_get|http_post)`,
 	}
+	// 正则匹配去掉php，一加手机：OnePlus Ace 2 (PHP110)。典型UserAgent: Mozilla/5.0 (Linux; U; Android 15; zh-cn; PHP110 Build/AP3A.240617.008) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/115.0.5790.168 Mobile Safari/537.36 HeyTapBrowser/40.10.2.1
 	var regexps []*regexp.Regexp
 	for _, pattern := range patterns {
 		if re, err := regexp.Compile(pattern); err == nil {
